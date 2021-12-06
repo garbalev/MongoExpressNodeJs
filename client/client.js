428c5532c80bb9e540e77a4217dc1424724db8a8
Alexander Garbalev <garbalevalex@mail.ru>
2021-12-07 00:46:28 +0300

    ajaxRequests

--- client/client.js
+++ client/client.js
@@ -38,24 +38,34 @@ let objForPut = { id: 4, name: "Serhio", age: 46 };
 function loadData(url = "") {
   $(".usersList").html("");
   $("#idValue")[0].value = "";
-  $.get(`http://localhost:3000/users-api/users${url}`).then((data) => {
-    console.log(data);
-    if (typeof data === "string") {
-      $(".usersList").append(`
+  $.get(`http://localhost:3000/users-api/users${url}`)
+    .then((data) => {
+      console.log(data);
+      if (typeof data === "string") {
+        $(".usersList").append(`
             <h1>${data}.</h1>
         `);
-    } else {
-      for (let user of data) {
-        $(".usersList").append(`
+      } else {
+        for (let user of data) {
+          $(".usersList").append(`
             <div class="user">
+              <div>  
                 <p>Name: <span class="response">${user.name}</span></p>
                 <p>Age: <span class="response">${user.age}</span></p>
                 <p>ID: <span class="response">${user.id}</span></p>
+              </div>
+              <div style='display: flex; flex-direction: column'>
+                <span class="buttons deleteUser">Delete user</span>
+                <span class="buttons editUser">Edit</span>
+              </div>           
             </div>
             `);
+        }
       }
-    }
-  });
+    })
+    .fail((err, status) =>
+      alert(status + ", check that server is running and the URL is correct")
+    );
 }
 
 $("#showAll").click(() => {
@@ -74,13 +84,58 @@ $("#showById").click(() => {
   }
 });
 
+// $(".deleteUser").click(() => {
+//   console.log('dasdadasddasnn');
+// });
+
+$(".usersList").on("click", ".deleteUser", function (e) {
+  $("#formAddUser").hide();
+  let deleteId = $(e.target)
+    .parent()
+    .siblings()
+    .find(".response:eq(-1)")
+    .text();
+  $("#formAddUser").hide();
+  $.ajax({
+    type: "DELETE",
+    url: `http://localhost:3000/users-api/users/${deleteId}`,
+    headers: { Accept: "application/json" },
+    success: (data) => console.log(data),
+  }).then(() => {
+    loadData();
+  });
+});
+
+$(".usersList").on("click", ".editUser", function (e) {
+  let deleteId = $(e.target).siblings().find(".response:eq(-1)").text();
+  console.log($(e.target).offset());
+  let topPos = $(e.target).parent().offset().top;
+  $("#addUser").text("Edit");
+  $("#formAddUser").show();
+  $("#formAddUser").offset({ top: topPos + 10, left: 262 });
+  // $("#formAddUser").css({
+  //   "left": "262",
+  //   "top": topPos + 1,
+  // });
+});
+
 $("#close").click(() => {
   $("#buttonAddUser").hide();
   $("#formAddUser").hide();
   $(".usersList").html("");
 });
 
-$("#buttonAddUser").click(() => $("#formAddUser").show());
+$("#buttonAddUser").click(() => {
+  if ($(".user").length > 0) {
+    $("#addUser").text("Add");
+    $("#formAddUser").css({
+      "left": "550px",
+      "top": "133px",
+    });
+    $("#formAddUser").show();
+    console.log($(".deleteUser"));
+  }
+});
 
 $("#addUser").click(() => {
   console.log(document.forms[0][0].value);
@@ -98,8 +153,12 @@ $("#addUser").click(() => {
       },
       data: JSON.stringify({ name: name, age: age }),
       success: (data) => console.log(data),
-    }).then(() => loadData());
+    })
+      .then(() => loadData())
+      .fail((err, status, errorThrown) =>
+        alert(status + ", check that server is running and the URL is correct")
+      );
   } else {
-      alert('Input fields must not be empty')
+    alert("Input fields must not be empty");
   }
 });
--- server/server.js
+++ server/server.js
@@ -1,100 +1,88 @@
-const express = require('express');
-const fs = require('fs/promises');
-
+const express = require("express");
+const fs = require("fs/promises");
 
 const app = express();
 const jsonParser = express.json();
-const path = 'users.json'
+const path = "users.json";
 
 // app.use(express.static(__dirname + '/files'));
 
-
-
-
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Request-Headers', '*');
-    next();
-})
-
-app.get('/users-api/users', (req, resp) => {
-    const result = fs.readFile(path, "utf-8")
-    .then(data => resp.send(JSON.parse(data)))
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Request-Headers", "*");
+  next();
 });
 
-app.get('/users-api/users/:id', (req, resp) => {
-    const reqId = req.params.id;
-    fs.readFile(path, "utf-8")
-    .then(data => {
-        const users = JSON.parse(data);
-        let result = 'No such user';
-        for (let us of users) {
-            if (us.id == reqId) {
-                result = [us];
-                break;
-            }
-        }
-        resp.send(result)
-    })
+app.get("/users-api/users", (req, resp) => {
+  const result = fs
+    .readFile(path, "utf-8")
+    .then((data) => resp.send(JSON.parse(data)));
 });
 
-app.post('/users-api/users', jsonParser, (req, resp) => {
-    const request = req;
-    const reqUserName = request.body.name;
-    const reqUserAge = request.body.age;
-    let newUser = {id: '', name: reqUserName, age: reqUserAge};
-    fs.readFile(path, 'utf-8')
-    .then(data => {
-        let users = JSON.parse(data);
-        const usersMaxId = Math.max(...users.map(el => el.id));
-        newUser.id = usersMaxId + 1;
-        users.push(newUser);
-        fs.writeFile(path, JSON.stringify(users))
-        .then(() => resp.send(users))
-    })
+app.get("/users-api/users/:id", (req, resp) => {
+  const reqId = req.params.id;
+  fs.readFile(path, "utf-8").then((data) => {
+    const users = JSON.parse(data);
+    let result = "No such user";
+    for (let us of users) {
+      if (us.id == reqId) {
+        result = [us];
+        break;
+      }
+    }
+    resp.send(result);
+  });
 });
 
-app.delete('/users-api/users/:id', (req, resp) => {
-    const deleteId = req.params.id;
-    fs.readFile(path, "utf-8")
-    .then(data => {
-        let users = JSON.parse(data);
-        let result = 'user with this ID doesn`t exist';
-        for (let user of users) {
-            if (user.id == deleteId) {
-                result = users.splice(users.indexOf(user), 1)[0];
-                break;  
-            } 
-        }
-        fs.writeFile(path, JSON.stringify(users))
-        .then(() => resp.send(result))
-
-    });
+app.post("/users-api/users", jsonParser, (req, resp) => {
+  const request = req;
+  const reqUserName = request.body.name;
+  const reqUserAge = request.body.age;
+  let newUser = { id: "", name: reqUserName, age: reqUserAge };
+  fs.readFile(path, "utf-8").then((data) => {
+    let users = JSON.parse(data);
+    const usersMaxId = Math.max(...users.map((el) => el.id));
+    newUser.id = usersMaxId + 1;
+    users.push(newUser);
+    fs.writeFile(path, JSON.stringify(users)).then(() => resp.send(users));
+  });
 });
 
-app.put('/users-api/users', jsonParser, (req, resp) => {
-    const reqId = req.body.id;
-    const reqName = req.body.name;
-    const reqAge = req.body.age;
-    fs.readFile(path, 'utf-8')
-    .then(data => {
-        let users = JSON.parse(data);
-        let result = 'user with this ID doesn`t exist';
-        for (let user of users) {
-            if (user.id == reqId) {
-                user.name = reqName;
-                user.age = reqAge;
-                result = user;
-                break
-            }
-        }
-        fs.writeFile(path, JSON.stringify(users))
-        .then(() => resp.send(result));
-    });
+app.delete("/users-api/users/:id", (req, resp) => {
+  const deleteId = req.params.id;
+  fs.readFile(path, "utf-8").then((data) => {
+    let users = JSON.parse(data);
+    for (let user of users) {
+      if (user.id == deleteId) {
+        result = users.splice(users.indexOf(user), 1)[0];
+        break;
+      }
+    }
+    fs.writeFile(path, JSON.stringify(users)).then(() => resp.send('done'));
+  });
 });
 
-app.listen(3000, function(){
-    console.log("Server started...");
+app.put("/users-api/users", jsonParser, (req, resp) => {
+  const reqId = req.body.id;
+  const reqName = req.body.name;
+  const reqAge = req.body.age;
+  fs.readFile(path, "utf-8").then((data) => {
+    let users = JSON.parse(data);
+    let result = "user with this ID doesn`t exist";
+    for (let user of users) {
+      if (user.id == reqId) {
+        user.name = reqName;
+        user.age = reqAge;
+        result = user;
+        break;
+      }
+    }
+    fs.writeFile(path, JSON.stringify(users)).then(() => resp.send(result));
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started...");
 });
